549d4d27d535164c1fd59e3f731e07a873d414d2
craignicol <craig.nicol@gmail.com>
2018-12-10 00:05:21 +0000

    put input sanitation in template

--- 2018/day.n.py
+++ 2018/day.n.py
@@ -3,7 +3,7 @@
 def execute():
     with open('input.1.txt') as inp:
         lines = inp.readlines()
-    return len(lines)
+    return len([l.strip() for l in lines if len(l.strip()) > 0])
 
 def verify(a, b):
     if (a == b):
