f337a2858e2ebf629fffc12d99272b6b7e58a6ac
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-12-01 17:23:53 +0000

    Day 1, Part 2

--- 2020/day.1.py
+++ 2020/day.1.py
@@ -3,7 +3,8 @@
 def execute():
     with open('2020/input/1.txt') as inp:
         lines = inp.readlines()
-    return multiply_to_2020([int(l.strip()) for l in lines if len(l.strip()) > 0])
+    numbers = [int(l.strip()) for l in lines if len(l.strip()) > 0]
+    return multiply_to_2020(numbers), multiply_3_to_2020(numbers)
 
 def verify(a, b):
     if (a == b):
@@ -19,8 +20,17 @@ def multiply_to_2020(number_list):
             return n * (2020-n)
         s.add(n)
 
+def multiply_3_to_2020(number_list):
+    s = set()
+    for n in number_list:
+        for x in s:
+            if (2020-n-x) in s:
+                return n * x * (2020-n-x)
+        s.add(n)
+
 def test_cases():
     verify(multiply_to_2020([1721,979,366,299,675,1456]), 514579)
+    verify(multiply_3_to_2020([1721,979,366,299,675,1456]), 241861950)
 
 if __name__ == "__main__":
     test_cases()
