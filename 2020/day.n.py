cea1e3c9cc090c9ffe2522fd5e913c965dc0af99
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-12-03 22:22:08 +0000

    Better test reporting

--- 2020/day.n.py
+++ 2020/day.n.py
@@ -5,11 +5,16 @@ def execute():
         lines = inp.readlines()
     return len([l.strip() for l in lines if len(l.strip()) > 0])
 
+tests_failed = 0
+tests_executed = 0
+
 def verify(a, b):
+    tests_executed = 1
     if (a == b):
         print("✓")
         return
     
+    tests_failed += 1
     print (locals())
 
 def test_cases():
@@ -17,4 +22,5 @@ def test_cases():
 
 if __name__ == "__main__":
     test_cases()
+    print("Failed " + tests_failed + " out of " + tests_executed + " tests. ")
     print(execute())
