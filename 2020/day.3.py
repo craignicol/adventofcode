f8d7950b2f26b0cbb916396a056db5c995feda77
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-12-03 21:00:33 +0000

    Day 2. Part 2

--- 2020/day.3.py
+++ 2020/day.3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from functools import reduce
 
 sample = """..##.......
 #...#...#..
@@ -10,12 +11,15 @@ sample = """..##.......
 .#........#
 #.##...#...
 #...##....#
-.#..#...#.#"""
+.#..#...#.#""".splitlines()
+
+slopes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 
 def execute():
     with open('2020/input/3.txt') as inp:
         lines = inp.readlines()
-    return count_trees([l.strip() for l in lines if len(l.strip()) > 0])
+    data = [l.strip() for l in lines if len(l.strip()) > 0]
+    return count_trees(data), multiply_trees(data)
 
 def verify(a, b):
     if (a == b):
@@ -24,19 +28,31 @@ def verify(a, b):
     
     print (locals())
 
-def count_trees(localmap):
+def count_trees(localmap, x_step = 3, y_step = 1):
+    if y_step < 1:
+        return 0
     # Move 3 right, 1 down
     x, y = 0, 0
     tree_count = 0
     while (y < len(localmap)):
         if localmap[y][x] == "#":
             tree_count += 1
-        x = (x + 3) % len(localmap[0])
-        y += 1
+        x = (x + x_step) % len(localmap[0])
+        y += y_step
     return tree_count
 
+def multiply_trees(localmap):
+    return reduce((lambda x, y: x * y), [count_trees(localmap, *slopes[i]) for i in range(len(slopes))])
+
 def test_cases():
-    verify(count_trees(sample.splitlines()), 7)
+    verify(count_trees(sample), 7)
+    verify(count_trees(sample, *slopes[0]), 2)
+    verify(count_trees(sample, *slopes[1]), 7)
+    verify(count_trees(sample, *slopes[2]), 3)
+    verify(count_trees(sample, *slopes[3]), 4)
+    verify(count_trees(sample, *slopes[4]), 2)
+    verify(multiply_trees(sample), 336)
+    
 
 if __name__ == "__main__":
     test_cases()
