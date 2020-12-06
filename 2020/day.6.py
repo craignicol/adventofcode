7746e49ebd345814724783d3bca792bc573ea658
Craig Nicol <craig.nicol@screenmedia.co.uk>
2020-12-06 23:11:32 +0000

    Day 6, Part 2

--- 2020/day.6.py
+++ 2020/day.6.py
@@ -20,10 +20,27 @@ a
 
 b"""
 
+group3 = """abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"""
+
 def execute():
     with open('2020/input/6.txt') as inp:
         lines = inp.readlines()
-    return yes_all_group_count(''.join(lines))
+    declaration = ''.join(lines)
+    return yes_any_group_count(declaration), yes_all_group_count(declaration)
 
 tests_failed = 0
 tests_executed = 0
@@ -40,18 +57,28 @@ def verify(a, b):
     tests_failed += 1
     print (locals())
 
-def yes_group_count(answers):
+def any_group_count(answers):
     any_yes = set()
     for person in answers.split():
         any_yes.update(person[:])
     return len(any_yes)
 
+def yes_any_group_count(allanswers):
+    return sum([any_group_count(a) for a in allanswers.split("\n\n")])
+
+def all_group_count(answers):
+    all_yes = set("abcdefghijklmnopqrstuvwxyz"[:])
+    for person in answers.split():
+        all_yes.intersection_update(person[:])
+    return len(all_yes)
+
 def yes_all_group_count(allanswers):
-    return sum([yes_group_count(a) for a in allanswers.split("\n\n")])
+    return sum([all_group_count(a) for a in allanswers.split("\n\n")])
 
 def test_cases():
-    verify(yes_group_count(group1), 6)
-    verify(yes_all_group_count(group2), 11)
+    verify(any_group_count(group1), 6)
+    verify(yes_any_group_count(group2), 11)
+    verify(yes_all_group_count(group3), 6)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
