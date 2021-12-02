f4a9cd561424d2e526edbe114c9f04f89d0cba2b
Craig Nicol <craig.nicol@gmail.com>
2021-12-02 14:11:39 +0000

    2021, Day 1, Part 2

--- 2021/day.1.py
+++ 2021/day.1.py
@@ -3,7 +3,8 @@
 def execute():
     with open('./input/day.1.txt') as inp:
         lines = inp.readlines()
-    return count_increasing([l.strip() for l in lines if len(l.strip()) > 0])
+    data = [l.strip() for l in lines if len(l.strip()) > 0]
+    return count_increasing(data), count_increasing(data, 3)
 
 tests_failed = 0
 tests_executed = 0
@@ -33,20 +34,23 @@ example1 = [
 263
 ]
 
-def count_increasing(list_num):
+def count_increasing(list_num, window_size = 1):
     list_num = [int(n) for n in list_num]
-    last_num = -1
-    increasing_count = -1 # First is always bigger
+    last_num = [-1] * window_size
+    last_sum = sum(last_num)
+    increasing_count = -window_size # First is always bigger
 
     for i in list_num:
-        if i > last_num:
+        last_num = last_num[1:] + [i]
+        if sum(last_num) > last_sum:
             increasing_count += 1
-        last_num = i
+        last_sum = sum(last_num)
 
     return increasing_count
 
 def test_cases():
     verify(count_increasing(example1), 7)
+    verify(count_increasing(example1, 3), 5)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
