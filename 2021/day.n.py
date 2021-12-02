301f0c6f3a0b3a3ecf26eae554e2da9d1b59f7f8
Craig Nicol <craig.nicol@gmail.com>
2021-12-02 13:54:02 +0000

    2021, day n setup

---
+++ 2021/day.n.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def execute():
+    with open('2021/input/1.txt') as inp:
+        lines = inp.readlines()
+    return len([l.strip() for l in lines if len(l.strip()) > 0])
+
+tests_failed = 0
+tests_executed = 0
+
+def verify(a, b):
+    global tests_executed
+    global tests_failed
+
+    tests_executed += 1
+    if (a == b):
+        print("✓")
+        return
+    
+    tests_failed += 1
+    print (locals())
+
+def test_cases():
+    print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
+
+if __name__ == "__main__":
+    test_cases()
+    print(execute())
