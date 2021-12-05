b3578b39c7f55bc4f1ceca308e4a89f56bbf78ee
Craig Nicol <craig.nicol@gmail.com>
2021-12-05 22:38:55 +0000

    Day 5, Part 1

--- 2021/day.5.py
+++ 2021/day.5.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+
 def execute():
     with open('./input/day.5.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return len(data)
+    return count_overlaps(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -30,23 +32,32 @@ example1 = """0,9 -> 5,9
 0,9 -> 2,9
 3,4 -> 1,4
 0,0 -> 8,8
-5,5 -> 8,2"""
+5,5 -> 8,2""".splitlines()
 
+def get_range(start, end):
+    x_dir = 1 if end[0] >= start[0] else -1
+    y_dir = 1 if end[1] >= start[1] else -1
+    return [(x,y) for x in range(start[0], end[0]+x_dir, x_dir) for y in range(start[1], end[1]+y_dir, y_dir)]
 
 def count_overlaps(data):
     """Note that this currently discards diagonals"""
-    overlaps = 0
+    vents_sparse = defaultdict(int)
     for line in data:
         line = line.split(' -> ')
         start = line[0].split(',')
         end = line[1].split(',')
         start = (int(start[0]), int(start[1]))
         end = (int(end[0]), int(end[1]))
-        if start[0] == end[0] or start[1] == end[1]:
-            overlaps += 1
-    return overlaps
+        if start[0] == end[0] or start[1] == end[1]: # Horizontal or vertical
+            for x,y in get_range(start, end):
+                vents_sparse[(x,y)] += 1
+        else:
+            print("Skipping line: {}".format(line))
+    print(vents_sparse)
+    return len([v for v in vents_sparse.values() if v > 1])
 
 def test_cases():
+    verify(count_overlaps(example1), 5)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
