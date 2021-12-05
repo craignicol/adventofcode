5d96d4088c59ea6342567a43d5767c006bc2887e
Craig Nicol <craig.nicol@gmail.com>
2021-12-05 22:47:27 +0000

    Day 5, Part 2

--- 2021/day.5.py
+++ 2021/day.5.py
@@ -6,7 +6,7 @@ def execute():
     with open('./input/day.5.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return count_overlaps(data)
+    return count_overlaps(data, ignore_diag=True), count_overlaps(data, ignore_diag=False)
 
 tests_failed = 0
 tests_executed = 0
@@ -37,9 +37,11 @@ example1 = """0,9 -> 5,9
 def get_range(start, end):
     x_dir = 1 if end[0] >= start[0] else -1
     y_dir = 1 if end[1] >= start[1] else -1
-    return [(x,y) for x in range(start[0], end[0]+x_dir, x_dir) for y in range(start[1], end[1]+y_dir, y_dir)]
+    x_range = range(start[0], end[0] + x_dir, x_dir) if start[0] != end[0] else [start[0]] * (abs(end[1] - start[1]) + 1)
+    y_range = range(start[1], end[1] + y_dir, y_dir) if start[1] != end[1] else [start[1]] * (abs(end[0] - start[0]) + 1)
+    return [(x,y) for (x,y) in zip(x_range, y_range)]
 
-def count_overlaps(data):
+def count_overlaps(data, ignore_diag):
     """Note that this currently discards diagonals"""
     vents_sparse = defaultdict(int)
     for line in data:
@@ -48,16 +50,14 @@ def count_overlaps(data):
         end = line[1].split(',')
         start = (int(start[0]), int(start[1]))
         end = (int(end[0]), int(end[1]))
-        if start[0] == end[0] or start[1] == end[1]: # Horizontal or vertical
+        if start[0] == end[0] or start[1] == end[1] or not ignore_diag: # Horizontal or vertical
             for x,y in get_range(start, end):
                 vents_sparse[(x,y)] += 1
-        else:
-            print("Skipping line: {}".format(line))
-    print(vents_sparse)
     return len([v for v in vents_sparse.values() if v > 1])
 
 def test_cases():
-    verify(count_overlaps(example1), 5)
+    verify(count_overlaps(example1, ignore_diag=True), 5)
+    verify(count_overlaps(example1, ignore_diag=False), 12)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
