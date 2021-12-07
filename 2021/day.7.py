6fbc10da7afdfb9d9aefd990a74453d2e9817134
Craig Nicol <craig.nicol@gmail.com>
2021-12-07 23:56:08 +0000

    Day 7, Part 1

---
+++ 2021/day.7.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+def execute():
+    with open('./input/day.7.txt') as inp:
+        lines = inp.readlines()
+    data = [l.strip() for l in lines if len(l.strip()) > 0]
+    return least_fuel(data[0])
+
+tests_failed = 0
+tests_executed = 0
+
+def verify(a, b):
+    global tests_executed
+    global tests_failed
+
+    tests_executed += 1
+    if (a == b):
+        print("✓")
+        return
+    
+    tests_failed += 1
+    print (locals())
+
+example1 = """16,1,2,0,4,2,7,1,2,14"""
+
+def calculate_fuel(x, positions):
+    return sum([abs(x-p) for p in positions])
+
+def least_fuel(current_positions):
+    positions = [int(x) for x in current_positions.split(',')]
+    fuel_costs = []
+    for x in range(min(positions), max(positions)):
+        fuel_costs.append(calculate_fuel(x, positions))
+    return min(fuel_costs)
+
+def test_cases():
+    verify(least_fuel(example1), 37)
+    print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
+
+if __name__ == "__main__":
+    test_cases()
+    print(execute())
