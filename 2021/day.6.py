65fd1dc82d59e8a27e7a3a82816f83779362da69
Craig Nicol <craig.nicol@gmail.com>
2021-12-07 10:09:43 +0000

    Day 6, Part 2 (incomplete)

--- 2021/day.6.py
+++ 2021/day.6.py
@@ -23,26 +23,37 @@ def verify(a, b):
 
 example1 = """3,4,3,1,2"""
 
-def grow_lanternfish(state):
-    new_state = []
-    for fish in state:
-        if fish == 0:
-            new_state.append(6) # adults have a cycle of 6
-            new_state.append(8) # newborns have a cycle of 8
-        else:
-            new_state.append(fish - 1)
-    return new_state
+# 5 5 6 7 9 10 10 10 10 11 12 15 17 19 20 20 21 22 26
 
 def count_lanternfish(initial_state, days):
     state = [int(x) for x in initial_state.split(',')]
-    for _ in range(days):
-        state = grow_lanternfish(state)
-    return len(state)
+    count = len(state)
+    for i in range(days):
+        count += state.count(i % 6) + state.count(i % 8)
+    return count
 
 def test_cases():
+    verify(count_lanternfish(example1, 0), 5)
     verify(count_lanternfish(example1, 1), 5)
+    verify(count_lanternfish(example1, 2), 6)
+    verify(count_lanternfish(example1, 3), 7)
+    verify(count_lanternfish(example1, 4), 9)
+    verify(count_lanternfish(example1, 5), 10)
+    verify(count_lanternfish(example1, 6), 10)
+    verify(count_lanternfish(example1, 7), 10)
+    verify(count_lanternfish(example1, 8), 10)
+    verify(count_lanternfish(example1, 9), 11)
+    verify(count_lanternfish(example1, 10), 12)
+    verify(count_lanternfish(example1, 11), 15)
+    verify(count_lanternfish(example1, 12), 17)
+    verify(count_lanternfish(example1, 13), 19)
+    verify(count_lanternfish(example1, 14), 10)
+    verify(count_lanternfish(example1, 15), 20)
+    verify(count_lanternfish(example1, 16), 21)
+    verify(count_lanternfish(example1, 17), 22)
     verify(count_lanternfish(example1, 18), 26)
     verify(count_lanternfish(example1, 80), 5934)
+    verify(count_lanternfish(example1, 256), 26984457539)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
