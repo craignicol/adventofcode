dcadcbfc3cbb9a83f034ccf63d4bb9293427a9f4
Craig Nicol <craig.nicol@gmail.com>
2021-12-08 21:29:15 +0000

    Day 8, Part 2

--- 2021/day.8.py
+++ 2021/day.8.py
@@ -4,7 +4,7 @@ def execute():
     with open('./input/day.8.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return count_unique_number_segments(data)
+    return count_unique_number_segments(data), sum_all_outputs(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -20,6 +20,8 @@ def verify(a, b):
     
     tests_failed += 1
     print (locals())
+
+example0 = """acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf""".splitlines()
 
 example1 = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -46,8 +48,67 @@ def split_segments(lines):
         segments.append( (i.split(' '), o.split(' ')) )
     return segments
 
+bin_value = {'a': 1, 'b': 2, 'c': 4, 'd': 8, 'e': 16, 'f': 32, 'g': 64}
+
+def get_hash(wires): #abcdefg
+    return sum([bin_value[w] for w in wires])
+
+def calculate_hash_segments(segments):
+    h = {}
+    lookup = {}
+    for s in segments:
+        if len(s) == 2:
+            h[get_hash(s)] = 1
+            lookup[1] = get_hash(s)
+        elif len(s) == 3:
+            h[get_hash(s)] = 7
+            lookup[7] = get_hash(s)
+        elif len(s) == 4:
+            h[get_hash(s)] = 4
+            lookup[4] = get_hash(s)
+        elif len(s) == 7:
+            h[get_hash(s)] = 8
+            lookup[8] = get_hash(s)
+
+    for s in segments:
+        if get_hash(s) in h:
+            continue
+        elif len(s) == 5 and (get_hash(s) & lookup[7]) == lookup[7]:
+            h[get_hash(s)] = 3
+            lookup[3] = get_hash(s)
+        elif len(s) == 6 and (get_hash(s) & lookup[4]) == lookup[4]:
+            h[get_hash(s)] = 9
+            lookup[9] = get_hash(s)
+        elif len(s) == 6 and (get_hash(s) & lookup[7]) == lookup[7]:
+            h[get_hash(s)] = 0
+            lookup[0] = get_hash(s)
+        elif len(s) == 6:
+            h[get_hash(s)] = 6
+            lookup[6] = get_hash(s)
+
+    for s in segments:
+        if get_hash(s) in h:
+            continue
+        if len(s) == 5 and (get_hash(s) & lookup[6]) == get_hash(s):
+            h[get_hash(s)] = 5
+            lookup[5] = get_hash(s)
+        else:
+            h[get_hash(s)] = 2
+            lookup[2] = get_hash(s)
+    return h
+
+def get_output(segment):
+    i, o = segment
+    h = calculate_hash_segments(i)
+    return (1000 * h[get_hash(o[0])]) + (100 * h[get_hash(o[1])]) + (10 * h[get_hash(o[2])]) + h[get_hash(o[3])]
+
+def sum_all_outputs(lines):
+    return sum([get_output(s) for s in split_segments(lines)])
+
 def test_cases():
     verify(count_unique_number_segments(example1), 26)
+    verify(get_output(split_segments(example0)[0]), 5353)
+    verify(sum_all_outputs(example1), 61229)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
