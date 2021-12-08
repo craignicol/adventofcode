aba5bf377f4d4f89aa221242993210880402c9cc
Craig Nicol <craig.nicol@gmail.com>
2021-12-08 00:04:17 +0000

    Day 7, Part 2

--- 2021/day.7.py
+++ 2021/day.7.py
@@ -4,7 +4,7 @@ def execute():
     with open('./input/day.7.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return least_fuel(data[0])
+    return least_fuel(data[0]), least_fuel(data[0], calculate_fuel_triangle)
 
 tests_failed = 0
 tests_executed = 0
@@ -23,18 +23,25 @@ def verify(a, b):
 
 example1 = """16,1,2,0,4,2,7,1,2,14"""
 
+def triangle(n):
+    return n*(n+1)//2
+
+def calculate_fuel_triangle(x, positions):
+    return sum([triangle(abs(x-p)) for p in positions])
+
 def calculate_fuel(x, positions):
     return sum([abs(x-p) for p in positions])
 
-def least_fuel(current_positions):
+def least_fuel(current_positions, fuel_fn=calculate_fuel):
     positions = [int(x) for x in current_positions.split(',')]
     fuel_costs = []
     for x in range(min(positions), max(positions)):
-        fuel_costs.append(calculate_fuel(x, positions))
+        fuel_costs.append(fuel_fn(x, positions))
     return min(fuel_costs)
 
 def test_cases():
-    verify(least_fuel(example1), 37)
+    verify(least_fuel(example1, calculate_fuel), 37)
+    verify(least_fuel(example1, calculate_fuel_triangle), 168)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
