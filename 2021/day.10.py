874e4b250c4b3074d3e3be2040e347f77a23b62b
Craig Nicol <craig.nicol@gmail.com>
2021-12-10 22:52:05 +0000

    Day 10, Part 2

--- 2021/day.10.py
+++ 2021/day.10.py
@@ -4,7 +4,7 @@ def execute():
     with open('./input/day.10.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return syntax_error_score(data)
+    return syntax_error_score(data), autocomplete_score(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -36,8 +36,9 @@ example1 = """[({(<(())[]>[[{[]{<()<>>
 
 match_brackets = { '>' : '<', '<' : '>', ')' : '(', '(' : ')', ']' : '[', '[' : ']', '{' : '}', '}' : '{' }
 
-def find_corrupted_chars(data):
+def compiler(data):
     corrupted_chars = []
+    autocomplete = []
     for line in data:
         bracket_stack = []
         for i in line:
@@ -50,14 +51,35 @@ def find_corrupted_chars(data):
                     opener = bracket_stack.pop()
                     if match_brackets[i] != opener:
                         corrupted_chars.append(i)
-    return corrupted_chars
+                        bracket_stack = [] # Can't autocomplete
+                        break
+        autocomplete.append(''.join([match_brackets[b] for b in bracket_stack[::-1]]))
+    return corrupted_chars, autocomplete
 
 def syntax_error_score(data):
-    corrupted_chars = find_corrupted_chars(data)
+    corrupted_chars, _ = compiler(data)
     return sum([scores[c] for c in corrupted_chars])
+
+ascore = {')': 1, ']': 2, '}': 3, '>': 4}
+
+def acscore(c):
+    score = 0
+    for b in c:
+        score *= 5
+        score += ascore[b]
+    return score 
+
+def autocomplete_score(data):
+    _, autocomplete = compiler(data)
+    line_scores =  [acscore(c) for c in autocomplete if len(c) > 0]
+    # find median
+    line_scores.sort()
+    return line_scores[len(line_scores) // 2]
 
 def test_cases():
     verify(syntax_error_score(example1), 26397)
+    verify(compiler(example1[0:1])[1], ['}}]])})]'])
+    verify(autocomplete_score(example1), 288957)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
