bb64e6ddba8fdad5fc3326b88b59ac48a730fdd8
Craig Nicol <craig.nicol@gmail.com>
2021-12-19 00:49:09 +0000

    Day 13, Part 1

--- 2021/day.13.py
+++ 2021/day.13.py
@@ -4,7 +4,8 @@ def execute():
     with open('./input/day.13.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return len(data)
+    paper = fold_paper(parse_origami(data))
+    return render(paper), count_dots(paper)
 
 tests_failed = 0
 tests_executed = 0
@@ -64,8 +65,24 @@ def fold_paper(origami):
             elif axis == 'x':
                 new_dots.add((x if x < pos else pos + pos - x, y))
         dots = new_dots
+        print(len(dots))
+        # render((dots, folds))
         new_dots = set()
     return dots, folds
+
+def render(origami):
+    dots, _ = origami
+    min_x = min(d[0] for d in dots)
+    max_x = max(d[0] for d in dots)
+    min_y = min(d[1] for d in dots)
+    max_y = max(d[1] for d in dots)
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            if (x,y) in dots:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
 
 def count_dots(origami):
     dots, _ = origami
@@ -73,7 +90,7 @@ def count_dots(origami):
 
 def test_cases():
     verify(count_dots(parse_origami(example1)), 18)
-    verify(count_dots(fold_paper(parse_origami(example1))), 17)
+    verify(count_dots(fold_paper(parse_origami(example1))), 16)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
