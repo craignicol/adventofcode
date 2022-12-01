a53f922e6c9b4e3239ee521deddd2bcde2304efb
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2022-12-01 12:34:08 +0000

    2022, day 1b

--- 2022/day.1.py
+++ 2022/day.1.py
@@ -4,7 +4,7 @@ def execute():
     with open('./input/day.1.txt') as inp:
         lines = inp.readlines()
     data = "".join(lines)
-    return total_per_elf(data), most_calories(data)
+    return most_calories(data), top_three_total(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -44,9 +44,13 @@ def total_per_elf(elves_food):
 def most_calories(elves_food):
     return max(total_per_elf(elves_food))
 
+def top_three_total(elves_food):
+    return sum(sorted(total_per_elf(elves_food), reverse=True)[:3])
+
 def test_cases():
     verify(total_per_elf(elves_food), [6000, 4000, 11000, 24000, 10000])
     verify(most_calories(elves_food), 24000)
+    verify(top_three_total(elves_food), 45000)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
