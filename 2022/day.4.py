18f238dd96f4f1baae0ee70e5c6bf2853e8f08e5
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2022-12-04 14:39:41 +0000

    2022 day 4b

--- 2022/day.4.py
+++ 2022/day.4.py
@@ -4,7 +4,7 @@ def execute():
     with open('./input/day.4.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return count_fully_enclosed(data)
+    return count_fully_enclosed(data), count_overlap(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -42,6 +42,19 @@ def fully_enclosed(pair):
 def count_fully_enclosed(data):
     return len([d for d in data if fully_enclosed(d)])
 
+def overlap(pair):
+    a, b = pair.split(",")
+    a_start, a_end = a.split("-")
+    b_start, b_end = b.split("-")
+    a_start = int(a_start)
+    a_end = int(a_end)
+    b_start = int(b_start)
+    b_end = int(b_end)
+    return (a_start <= b_start and a_end >= b_start) or (b_start <= a_start and b_end >= a_start)
+
+def count_overlap(data):
+    return len([d for d in data if overlap(d)])
+
 def test_cases():
     verify(fully_enclosed("2-4,6-8"), False)
     verify(fully_enclosed("2-3,4-5"), False)
@@ -50,6 +63,13 @@ def test_cases():
     verify(fully_enclosed("6-6,4-6"), True)
     verify(fully_enclosed("2-6,4-8"), False)
     verify(count_fully_enclosed(sample_input), 2)
+    verify(overlap("2-4,6-8"), False)
+    verify(overlap("2-3,4-5"), False)
+    verify(overlap("5-7,7-9"), True)
+    verify(overlap("2-8,3-7"), True)
+    verify(overlap("6-6,4-6"), True)
+    verify(overlap("2-6,4-8"), True)
+    verify(count_overlap(sample_input), 4)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
