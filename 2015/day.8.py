573acb17b047f0e49a74fe0a99c2a9b3a69be908
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2022-12-04 15:35:59 +0000

    2015 Day 8b

--- 2015/day.8.py
+++ 2015/day.8.py
@@ -4,7 +4,8 @@ import re
 def execute():
     with open('2015/input/8.txt') as inp:
         lines = inp.readlines()
-    return length_diff([l.strip() for l in lines if len(l.strip()) > 0])
+    data = [l.strip() for l in lines if len(l.strip()) > 0]
+    return length_diff(data), length_diff([expanded(i) for i in data])
 
 tests_failed = 0
 tests_executed = 0
@@ -35,12 +36,20 @@ def lengths(input):
 def length_diff(input):
     return sum([l[0] - l[1] for l in [lengths(i) for i in input]])
 
+def expanded(input):
+    return '"' + input.replace("\\", "\\\\").replace('"', '\\"') + '"'
+
 def test_cases():
     verify(lengths(sample_input[0]), (2, 0))
     verify(lengths(sample_input[1]), (5, 3))
     verify(lengths(sample_input[2]), (10, 7))
     verify(lengths(sample_input[3]), (6, 1))
     verify(length_diff(sample_input), 12)
+    verify(expanded(sample_input[0]), r'"\"\""')
+    verify(expanded(sample_input[1]), r'"\"abc\""')
+    verify(expanded(sample_input[2]), r'"\"aaa\\\"aaa\""')
+    verify(expanded(sample_input[3]), r'"\"\\x27\""')
+    verify(length_diff([expanded(i) for i in sample_input]), 19)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
