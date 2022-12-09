c41e39366cbf80450562cf44a6625cc8ff103913
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2022-12-09 09:26:39 +0000

    2022, Day 9, incomplete

---
+++ 2022/day.9.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+def execute():
+    with open('2022/input/day.9.txt') as inp:
+        lines = inp.readlines()
+    data = [l.strip() for l in lines if len(l.strip()) > 0]
+    return tail_positions(data)
+
+tests_failed = 0
+tests_executed = 0
+
+def verify(a, b):
+    global tests_executed
+    global tests_failed
+
+    tests_executed += 1
+    if (a == b):
+        print("✓")
+        return
+    
+    tests_failed += 1
+    print (locals())
+
+sample_input = """
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2""".splitlines()
+
+visited = set()
+
+def sign(x):
+    if x == 0:
+        return 0
+    return x // abs(x)
+
+def tail_positions(moves):
+    x = 0
+    y = 0
+    head = (x,y)
+    tail = (x,y)
+    visited.add(tail)
+    for move in moves:
+        if len(move.strip()) == 0:
+            continue
+        move = move.split()
+        direction = move[0]
+        distance = int(move[1])
+        for i in range(distance):
+            # move the head
+            if direction == "R":
+                x += 1
+            elif direction == "L":
+                x -= 1
+            elif direction == "U":
+                y += 1
+            elif direction == "D":
+                y -= 1
+
+            # move the tail based on where the head was
+            xdiff, ydiff = head[0] - tail[0], head[1] - tail[1]
+            if abs(xdiff) > 1 or abs(ydiff) > 1: 
+                tail = (tail[0] + sign(xdiff), tail[1] + sign(ydiff))
+                visited.add(tail)
+
+            head = (x,y)
+    return len(visited)
+
+def test_cases():
+    verify(tail_positions([]),1)
+    verify(tail_positions(sample_input),13)
+    print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
+
+if __name__ == "__main__":
+    test_cases()
+    print(execute())
