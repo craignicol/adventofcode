5c8c15eb404a3be82174b540dd5ca974d8b4ace9
Craig Nicol <Craig.Nicol@screenmedia.co.uk>
2022-12-16 01:06:30 +0000

    2022, Day 14a

--- 2022/day.14.py
+++ 2022/day.14.py
@@ -4,7 +4,7 @@ def execute():
     with open('2022/input/day.14.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return len(data)
+    return drop_all_sand(data)[1]
 
 tests_failed = 0
 tests_executed = 0
@@ -43,6 +43,8 @@ def parse_walls(walls):
             miny = max(miny, y)
             thiswall.append((x, y))
         corners.append(thiswall)
+    minx -= 1
+    maxx += 1
     grid = [[False] * (maxx - minx + 1) for _ in range(miny + 1)]
     for wall in corners:
         for i in range(len(wall) - 1):
@@ -55,7 +57,7 @@ def parse_walls(walls):
                 for x in range(min(x1,x2)-minx, max(x1,x2)-minx + 1):
                     grid[y1][x] = True
     sand_start = (500 - minx, 0)
-    return grid
+    return grid, sand_start
 
 def count_walls(walls):
     return sum(len([c for c in row if c]) for row in walls)
@@ -69,31 +71,46 @@ def drop_sand(walls):
     y, x = sand_start
     # print_grid(walls, (y,x))
     # print('---')
-    while not walls[x][y]:
-        x += 1
-    # print_grid(walls, (y,x))
-    # print('--')
-    if walls[x][y]:
-        if walls[x][y - 1] and walls[x][y + 1]:
-            x -= 1
-        elif not walls[x+1][y - 1]:
+    while x < len(walls) - 1:
+        if not walls[x+1][y]:
+            x += 1
+        elif y > 0 and not walls[x+1][y - 1]:
+            x += 1
             y -= 1
-        else:
+        elif y == 0:
+            break
+        elif y < len(walls[x+1]) - 1 and not walls[x+1][y + 1]:
+            x += 1
             y += 1
-    # print_grid(walls, (y,x))
-    # print('-')
-    walls[x][y] = True
-    return walls
+        else:
+            break
+    if x < len(walls) - 1:
+        walls[x][y] = True
+    else:
+        return walls, None
+    return walls, (y, x)
+
+def drop_all_sand(walls):
+    walls, sand_start = parse_walls(walls)
+    last = 0
+    sand_count = 0
+    while count_walls(walls) != last:
+        last = count_walls(walls)
+        walls, sand_start = drop_sand(walls)
+        sand_count += 1
+        # print_grid(walls, sand_start)
+        # print('---')
+    return walls, sand_count - 1
 
 def test_cases():
-    print_grid(parse_walls(sample_input))
-    verify(count_walls(parse_walls(sample_input)), 20)
-    print_grid(drop_sand(parse_walls(sample_input)))
-    verify(count_walls(drop_sand(parse_walls(sample_input))), 21)
-    w = parse_walls(sample_input)
-    for _ in range(10):
-        w = drop_sand(w)
-        print_grid(w)
+    start = parse_walls(sample_input)[0]
+    print_grid(start)
+    verify(count_walls(start), 20)
+    start, sand = drop_sand(start)
+    verify(count_walls(start), 21)
+    w, sand_count = drop_all_sand(sample_input)
+    print_grid(w)
+    verify(sand_count, 24)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
