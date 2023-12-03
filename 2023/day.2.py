cff3309d57aa4f2372cd9352d7d5de5b13eaca6e
craignicol <craig.nicol@gmail.com>
2023-12-03 00:04:11 +0000

    2023 Day 2b

--- 2023/day.2.py
+++ 2023/day.2.py
@@ -4,7 +4,7 @@ def execute():
     with open('2023/input/day.2.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return all_possible_games(data)
+    return all_possible_games(data), total_power_of(data)
 
 tests_failed = 0
 tests_executed = 0
@@ -42,9 +42,28 @@ def possible_game(game):
                 return (game_id, False)
     return (game_id, True)
 
+def minimum_cubes(game):
+    min_cubes = {'red': 0, 'green': 0, 'blue': 0}
+    game_id, rounds = game.split(': ')
+    game_id = int(game_id.replace('Game ', ''))
+    rounds = rounds.split('; ')
+    for r in rounds:
+        cubes = r.split(', ')
+        for c in cubes:
+            count, colour = c.split()
+            min_cubes[colour] = max(min_cubes[colour], int(count))
+    return min_cubes
+
+def power_of(game):
+    cubes = minimum_cubes(game)
+    return cubes['red'] * cubes['green'] * cubes['blue']
+
 def all_possible_games(games):
     validgames = [possible_game(g) for g in games]
     return sum([id for (id, valid) in validgames if valid])
+
+def total_power_of(games):
+    return sum([power_of(g) for g in games])
 
 def test_cases():
     sample = sample_input.splitlines()
@@ -54,6 +73,12 @@ def test_cases():
     verify(possible_game(sample[3]), (4, False))
     verify(possible_game(sample[4]), (5, True))
     verify(all_possible_games(sample), 8)
+    verify(power_of(sample[0]), 48)
+    verify(power_of(sample[1]), 12)
+    verify(power_of(sample[2]), 1560)
+    verify(power_of(sample[3]), 630)
+    verify(power_of(sample[4]), 36)
+    verify(total_power_of(sample), 2286)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
