b6a26d352751b4a8e0637331139a0c462b1ce14e
craignicol <craig.nicol@gmail.com>
2023-12-05 01:54:54 +0000

    2023 Day 3b (too low)

--- 2023/day.3.py
+++ 2023/day.3.py
@@ -4,7 +4,7 @@ def execute():
     with open('2023/input/day.3.txt') as inp:
         lines = inp.readlines()
     data = [l.strip() for l in lines if len(l.strip()) > 0]
-    return sum(part_numbers(data))
+    return sum(part_numbers(data)), sum(gear_ratios(data))
 
 tests_failed = 0
 tests_executed = 0
@@ -28,7 +28,7 @@ def find_symbols(line):
             locations.append(idx)
     return set(locations)
 
-def find_adjacent_numbers(line, symbols):
+def find_adjacent_numbers(line, symbols, with_position = False):
     numbers = []
     current = 0
     near_symbol = False
@@ -40,7 +40,7 @@ def find_adjacent_numbers(line, symbols):
             near_symbol = near_symbol or (idx in sym) 
         if c not in '0123456789':
             if near_symbol and in_number:
-                numbers.append(current)
+                numbers.append((current, idx - len(str(current)) - 1, idx + 1)) if with_position else numbers.append(current)
             current = 0
             in_number = False
             near_symbol = False
@@ -50,7 +50,7 @@ def find_adjacent_numbers(line, symbols):
                 in_number = True
                 near_symbol = near_symbol or ((idx-1) in sym) or (idx in sym) or ((idx + 1) in sym)
     if near_symbol and in_number and current > 0:
-        numbers.append(current)
+        numbers.append((current, len(line) - len(str(current)) - 1, len(line))) if with_position else numbers.append(current)
     return numbers
 
 def part_numbers(inp):
@@ -60,6 +60,31 @@ def part_numbers(inp):
         numbers.extend(find_adjacent_numbers(line, symbols))
         symbols = [symbols[1], symbols[2], find_symbols(inp[idx+2]) if idx < (len(inp) - 2) else set()]
     return numbers
+
+def find_stars(line):
+    locations = []
+    for idx, c in enumerate(line):
+        if c == '*':
+            locations.append(idx)
+    return set(locations)    
+
+def gear_ratios(inp):
+    star_locations = [set()] + [find_stars(line) for line in inp] + [set()]
+    numbers = []
+    ratios = []
+    for idx, line in enumerate(inp):
+        numbers.append(find_adjacent_numbers(line, star_locations[idx:idx+3], True))
+    #for s in star_locations[1:-1]:
+    numbers = [[]] + numbers + [[]]
+    for idx, row in enumerate(star_locations[1:-1]):
+        rnumbers = []
+        for r in numbers[idx:idx+3]:
+            rnumbers.extend(r)
+        for star in row:
+            parts = [n for (n, a, b) in rnumbers if a <= star and b >= star]
+            if len(parts) == 2:
+                ratios.append(parts[0] * parts[1])
+    return ratios
 
 sample_input = """467..114..
 ...*......
@@ -76,6 +101,8 @@ def test_cases():
     inp = sample_input.splitlines()
     verify(part_numbers(inp), [467, 35, 663, 617, 592, 755, 664, 598])
     verify(sum(part_numbers(inp)), 4361)
+    verify(gear_ratios(inp), [16345, 451490])
+    verify(sum(gear_ratios(inp)), 467835)
     print("Failed {} out of {} tests. ".format(tests_failed, tests_executed))
 
 if __name__ == "__main__":
